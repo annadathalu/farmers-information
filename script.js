29a8629f936423bbc7e3739476b53cdf7665c0f0
annadathalu <147304039+annadathalu@users.noreply.github.com>
2023-10-08 20:10:06 +0530

    Final script.js

--- script.js
+++ script.js
@@ -1,42 +1,101 @@
 document.addEventListener("DOMContentLoaded", function () {
     const farmerNameDropdown = document.getElementById("farmerNameDropdown");
     const farmerDetails = document.getElementById("farmerDetails");
+    const languageToggle = document.getElementById("languageToggle");
 
-    // Fetch data from the CSV file
-    fetch("Data_v2_mapped.csv")
-        .then((response) => response.text())
-        .then((data) => {
-            // Parse the CSV data into an array of objects
-            const dataArray = Papa.parse(data, { header: true }).data;
+    // Function to fetch data based on selected language
+    function fetchData(selectedLanguage) {
+        let fileName = "";
+        let columnNames = {};
 
-            // Populate the dropdown with Farmer Names from the CSV data
-            dataArray.forEach((item) => {
-                const option = document.createElement("option");
-                option.value = item["Farmer Name"];
-                option.textContent = item["Farmer Name"];
-                farmerNameDropdown.appendChild(option);
-            });
+        if (selectedLanguage === "eng") {
+            fileName = "Data_v2_mapped.csv";
+            // Define column names for English language
+            columnNames = {
+                farmerName: "Farmer Name",
+                phone: "Phone",
+                currentCrops: "Current Crops",
+                variants: "Variants",
+                farmSize: "Farm Size",
+                produceQuantity: "Quantity of produce",
+                farmLocation: "Farm Location",
+                otherDetails: "Other Details",
+            };
+        } else if (selectedLanguage === "tel") {
+            fileName = "Data_v3_mapped.xlsx";
+            // Define column names for Telugu language
+            columnNames = {
+                farmerName: "Farmer Name",
+                phone: "ఫోన్",
+                currentCrops: "ప్రస్తుత పంటలు",
+                variants: "వైవిధ్యాలు",
+                farmSize: "వ్యవసాయ పరిమాణం",
+                produceQuantity: "ఉత్పత్తి పరిమాణం",
+                farmLocation: "వ్యవసాయ స్థానం",
+                otherDetails: "ఇతర వివరాలు",
+            };
+        }
 
-            // Event listener for dropdown change
-            farmerNameDropdown.addEventListener("change", function () {
-                const selectedFarmer = this.value;
-                const selectedData = dataArray.find(
-                    (item) => item["Farmer Name"] === selectedFarmer
-                );
-                if (selectedData) {
-                    farmerDetails.innerHTML = `<p><strong>Phone:</strong> ${selectedData["Phone"]}</p>
-                                              <p><strong>Current Crops:</strong> ${selectedData["Current Crops"]}</p>
-                                              <p><strong>Variants:</strong> ${selectedData["Variants"]}</p>
-                                              <p><strong>Farm Size:</strong> ${selectedData["Farm Size"]}</p>
-                                              <p><strong>Quantity of produce:</strong> ${selectedData["Quantity of produce"]}</p>
-                                              <p><strong>Farm Location:</strong> ${selectedData["Farm Location"]}</p>
-                                              <p><strong>Other Details:</strong> ${selectedData["Other Details"]}</p>`;
+        fetch(fileName)
+            .then((response) => {
+                if (fileName.endsWith(".csv")) {
+                    return response.text();
+                } else if (fileName.endsWith(".xlsx")) {
+                    return response.arrayBuffer();
                 } else {
-                    farmerDetails.innerHTML = "";
+                    throw new Error("Unsupported file format");
                 }
+            })
+            .then((data) => {
+                let dataArray;
+                if (fileName.endsWith(".csv")) {
+                    dataArray = Papa.parse(data, { header: true }).data;
+                } else if (fileName.endsWith(".xlsx")) {
+                    const workbook = XLSX.read(data, { type: "array" });
+                    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                    dataArray = XLSX.utils.sheet_to_json(worksheet);
+                }
+
+                // Populate the dropdown with Farmer Names from the data
+                dataArray.forEach((item) => {
+                    const option = document.createElement("option");
+                    option.value = item[columnNames.farmerName];
+                    option.textContent = item[columnNames.farmerName];
+                    farmerNameDropdown.appendChild(option);
+                });
+
+                // Event listener for dropdown change
+                farmerNameDropdown.addEventListener("change", function () {
+                    const selectedFarmer = this.value;
+                    const selectedData = dataArray.find(
+                        (item) => item[columnNames.farmerName] === selectedFarmer
+                    );
+                    if (selectedData) {
+                        farmerDetails.innerHTML = `<p><strong>${columnNames.phone}:</strong> ${selectedData[columnNames.phone]}</p>
+                                                  <p><strong>${columnNames.currentCrops}:</strong> ${selectedData[columnNames.currentCrops]}</p>
+                                                  <p><strong>${columnNames.variants}:</strong> ${selectedData[columnNames.variants]}</p>
+                                                  <p><strong>${columnNames.farmSize}:</strong> ${selectedData[columnNames.farmSize]}</p>
+                                                  <p><strong>${columnNames.produceQuantity}:</strong> ${selectedData[columnNames.produceQuantity]}</p>
+                                                  <p><strong>${columnNames.farmLocation}:</strong> ${selectedData[columnNames.farmLocation]}</p>
+                                                  <p><strong>${columnNames.otherDetails}:</strong> ${selectedData[columnNames.otherDetails]}</p>`;
+                    } else {
+                        farmerDetails.innerHTML = "";
+                    }
+                });
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
             });
-        })
-        .catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+    }
+
+    // Event listener for language toggle change
+    languageToggle.addEventListener("change", function () {
+        const selectedLanguage = this.value;
+        farmerNameDropdown.innerHTML = ""; // Clear dropdown options
+        farmerDetails.innerHTML = ""; // Clear farmer details
+        fetchData(selectedLanguage);
+    });
+
+    // Initialize with default language (English)
+    fetchData("tel");
 });
